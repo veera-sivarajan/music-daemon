15366dc08ec62c0933cf6914403e7198e99a97be
Veera <sveera.2001@gmail.com>
2023-03-24 11:53:36 -0400

    Remove unnecessary daemonize options

--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::File;
+use std::fs::OpenOptions;
 
 use inotify::{
     Inotify,
@@ -9,17 +9,21 @@ use daemonize::Daemonize;
 use chrono::prelude::*;
 
 fn main() {
-    let stdout = File::create("/tmp/musicd-logs/musicd.out").unwrap();
-    let stderr = File::create("/tmp/musicd-logs/musicd.err").unwrap();
+    let stdout = OpenOptions::new()
+        .append(true)
+        .open("/tmp/musicd-logs/musicd.out")
+        .expect("Cannot open stdout file.");
+
+    let stderr = OpenOptions::new()
+        .append(true)
+        .open("/tmp/musicd-logs/musicd.err")
+        .expect("Cannot open stderr file.");
 
     let daemonize = Daemonize::new()
-        .pid_file("/tmp/musicd-logs/musicd.pid") // Every method except `new` and `start`
-        .working_directory("/tmp/musicd-logs/") // for default behaviour.
-        .umask(0o777)    // Set umask, `0o027` by default.
-        .stdout(stdout)  // Redirect stdout to `/tmp/daemon.out`.
-        .stderr(stderr);  // Redirect stderr to `/tmp/daemon.err`.
+        .working_directory("/tmp/musicd-logs/") 
+        .stdout(stdout)  
+        .stderr(stderr); 
 
-    
     let mut inotify = Inotify::init()
         .expect("Error while initializing inotify instance");
 
